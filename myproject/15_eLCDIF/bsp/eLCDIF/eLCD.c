1df4909bd0db149bfc6d867bb39865e5505eff75
Auqin <2480015679@qq.com>
2026-02-06 00:47:01 +0800

    eLCDIF 驱动开发进度 10%_编写初始化eLCDIF控制器中

--- myproject/15_eLCDIF/bsp/eLCDIF/eLCD.c
+++ myproject/15_eLCDIF/bsp/eLCDIF/eLCD.c
@@ -15,3 +15,151 @@
 
 
 
+void eLCDIF_Init(void) {
+
+    uint32_t ELCD_PAD_VALUE = 0;
+
+    // 1、Configure the external I/Os to correctly interface the external display
+    // 设置eLCDIF 外部IO 口 复用到eLCDIF功能:LCD_PCLK、LCD_VSYNC、LCD_HSYNC、LCD_DE、LCD_DATA0~23
+    // 设置eLCDIF 外部IO 口 复用到eLCDIF功能:LCD_PCLK、LCD_VSYNC、LCD_HSYNC、LCD_DE
+    IOMUXC_SetPinMux(IOMUXC_LCD_CLK_LCDIF_CLK, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_VSYNC_LCDIF_VSYNC, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_HSYNC_LCDIF_HSYNC, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_ENABLE_LCDIF_ENABLE, 0);
+    // 设置eLCDIF 外部IO 口 复用到eLCDIF功能:LCD_DATA0~23
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA00_LCDIF_DATA00, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA01_LCDIF_DATA01, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA02_LCDIF_DATA02, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA03_LCDIF_DATA03, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA04_LCDIF_DATA04, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA05_LCDIF_DATA05, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA06_LCDIF_DATA06, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA07_LCDIF_DATA07, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA08_LCDIF_DATA08, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA09_LCDIF_DATA09, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA10_LCDIF_DATA10, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA11_LCDIF_DATA11, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA12_LCDIF_DATA12, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA13_LCDIF_DATA13, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA14_LCDIF_DATA14, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA15_LCDIF_DATA15, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA16_LCDIF_DATA16, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA17_LCDIF_DATA17, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA18_LCDIF_DATA18, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA19_LCDIF_DATA19, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA20_LCDIF_DATA20, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA21_LCDIF_DATA21, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA22_LCDIF_DATA22, 0);
+    IOMUXC_SetPinMux(IOMUXC_LCD_DATA23_LCDIF_DATA23, 0);
+
+
+     /*配置 ELCD 的 IO 属性为 复用成为ELCDIF 的相关属性
+     *bit [16]:0 HYS 关闭
+     *bit [15:14]: 00 100K 下拉
+     *bit [13]: 0 kepper 功能
+     *bit [12]: 0 pull/keeper 功能  disable
+     *bit [11]: 0 关闭开路输出
+     *bit [7:6]: 10 速度 100Mhz
+     *bit [5:3]: 110 R0/6 驱动能力
+     *bit [0]: 0 低转换率
+     */
+
+    ELCD_PAD_VALUE = IOMUXC_SW_PAD_CTL_PAD_HYS(0)|\
+                     IOMUXC_SW_PAD_CTL_PAD_PUS(0)|\
+                     IOMUXC_SW_PAD_CTL_PAD_PUE(0)|\
+                     IOMUXC_SW_PAD_CTL_PAD_PKE(0)|\
+                     IOMUXC_SW_PAD_CTL_PAD_ODE(0)|\
+                     IOMUXC_SW_PAD_CTL_PAD_SPEED(2)|\
+                     IOMUXC_SW_PAD_CTL_PAD_DSE(6)|\
+                     IOMUXC_SW_PAD_CTL_PAD_SRE(0);
+        
+    //配置eLCDIF 外部IO pad 的属性:LCD_PCLK、LCD_VSYNC、LCD_HSYNC、LCD_DE
+    IOMUXC_SetPinConfig(IOMUXC_LCD_CLK_LCDIF_CLK, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_VSYNC_LCDIF_VSYNC, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_HSYNC_LCDIF_HSYNC, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_ENABLE_LCDIF_ENABLE, ELCD_PAD_VALUE);
+    //配置eLCDIF 外部IO pad 的属性:LCD_DATA0~23
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA00_LCDIF_DATA00, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA01_LCDIF_DATA01, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA02_LCDIF_DATA02, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA03_LCDIF_DATA03, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA04_LCDIF_DATA04, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA05_LCDIF_DATA05, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA06_LCDIF_DATA06, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA07_LCDIF_DATA07, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA08_LCDIF_DATA08, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA09_LCDIF_DATA09, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA10_LCDIF_DATA10, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA11_LCDIF_DATA11, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA12_LCDIF_DATA12, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA13_LCDIF_DATA13, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA14_LCDIF_DATA14, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA15_LCDIF_DATA15, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA16_LCDIF_DATA16, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA17_LCDIF_DATA17, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA18_LCDIF_DATA18, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA19_LCDIF_DATA19, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA20_LCDIF_DATA20, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA21_LCDIF_DATA21, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA22_LCDIF_DATA22, ELCD_PAD_VALUE);
+    IOMUXC_SetPinConfig(IOMUXC_LCD_DATA23_LCDIF_DATA23, ELCD_PAD_VALUE);
+
+
+    //2. Start the DISPLAY CLOCK (pix_clk) clock and set the appropriate frequency by programming the registers in CCM.
+    
+    /*------------------------
+    -----------ATK7016--------------
+    ---------水平显示区域 1024 tCLK-
+    ---------HSPW(thp) 20 tCLK------
+    ---------HBP(thb) 140 tCLK------
+    ---------HFP(thf) 160 tCLK------
+    ---------垂直显示区域 600 th----
+    ---------VSPW(tvp) 3 th---------
+    ---------VBP(tvb) 20 th---------
+    ---------VFP(tvf) 12 th---------
+    ---------像素时钟 51.2 MHz------*/
+    //显示一帧图像需要的时钟为: (VSPW+VBP+LINE+VFP) * (HSPW + HBP + HOZVAL + HFP) =  (3 + 20 + 600 + 12) * (20 + 140 + 1024 + 160) = 853440
+    //显示60帧图像需要的时钟为: 853440 * 60 = 51.2MHz,所以目标 pix_clk = 51.2MHz
+    //首先配置LCDIF1_CLK_ROOT的时钟为PLL5  pix_clk from LCDIF1_CLK_ROOT 因此将PLL5的时钟配置为51.2MHz
+    SET_BIT(CCM->CSCDR2, CCM_CSCDR2_LCDIF1_PRE_CLK_SEL_MASK, CCM_CSCDR2_LCDIF1_PRE_CLK_SEL(2));//010 derive clock from PLL5
+    SET_BIT(CCM->CSCDR2, CCM_CSCDR2_LCDIF1_PRED_MASK, CCM_CSCDR2_LCDIF1_PRED(0));//Pre-divider for lcdif1 clock. 000 divide by 1
+    SET_BIT(CCM->CBCMR,  CCM_CBCMR_LCDIF1_PODF_MASK, CCM_CBCMR_LCDIF1_PODF(0));//Post-divider for LCDIF1 clock. 000 divide by 1
+    SET_BIT(CCM->CSCDR2, CCM_CSCDR2_LCDIF1_CLK_SEL_MASK, CCM_CSCDR2_LCDIF1_CLK_SEL(0));//000 derive clock from divided pre-muxed LCDIF1 clock
+
+    // 配置PLL5(Video PLL)的时钟(reference manual(IMX6ULL):18.5.1.3.4 Audio / Video PLL)
+    // The output frequency can be set by programming the fields
+    // in the CCM_ANALOG_PLL_AUDIO, CCM_ANALOG_PLL_VIDEO, and
+    // CCM_ANALOG_MISC2 register sets according to the following equation
+    // PLL output frequency = Fref * (DIV_SELECT + NUM/DENOM) = 24*()
+    
+    SET_BIT(CCM_ANALOG->PLL_VIDEO, CCM_ANALOG_PLL_AUDIO_POST_DIV_SELECT_MASK, CCM_ANALOG_PLL_AUDIO_POST_DIV_SELECT(2));//10 — Divide by 1
+    /**DEBUG:上电默认是1:旁路Video PLL,我这里取消旁路**/
+    SET_BIT(CCM_ANALOG->PLL_VIDEO, CCM_ANALOG_PLL_AUDIO_BYPASS_CLK_SRC_MASK, CCM_ANALOG_PLL_AUDIO_BYPASS_CLK_SRC(0));//0 — Don't Bypass the PLL
+    SET_BIT(CCM_ANALOG->PLL_VIDEO, CCM_ANALOG_PLL_AUDIO_ENABLE_MASK, CCM_ANALOG_PLL_AUDIO_ENABLE(1));//1 — Enalbe PLL output
+    /**DEBUG:上电默认是1:Powers down the PLL.**/
+    SET_BIT(CCM_ANALOG->PLL_VIDEO, CCM_ANALOG_PLL_AUDIO_POWERDOWN_MASK, CCM_ANALOG_PLL_AUDIO_POWERDOWN(0));//0 — Don't Powers down the PLL.
+    SET_BIT(CCM_ANALOG->PLL_VIDEO, CCM_ANALOG_PLL_AUDIO_DIV_SELECT_MASK, CCM_ANALOG_PLL_AUDIO_DIV_SELECT(27));// Valid range for DIV_SELECT divider value: 27~54.
+
+    SET_BIT(CCM_ANALOG->MISC2, CCM_ANALOG_MISC2_VIDEO_DIV_MASK, CCM_ANALOG_MISC2_VIDEO_DIV(0));// 00 divide by 1 (Default)
+
+    //还有NUM/DENOM 没有配置 明天再配置
+
+    
+    //3. Start the BUS CLOCK (apb_clk) and set the appropriate frequency by programmingthe registers in CCM.
+
+
+
+
+
+}
+
+
+
+void BSP_eLCDIF_Init(ELCD_HandleTypeDef *ElcdHandle) {
+
+
+
+
+
+
+}
--- myproject/15_eLCDIF/bsp/eLCDIF/eLCD.h
+++ myproject/15_eLCDIF/bsp/eLCDIF/eLCD.h
@@ -5,17 +5,59 @@
 #include "stdio.h"
 
 
+typedef struct __ELCD_Init
+{
+
+
+
+}ELCD_Init_t;
+
+
+typedef struct __ELCD_IT_Config
+{
+
+
+
+}ELCD_IT_Config_t;
+
+
+/**
+ * @brief  UART handle Structure definition
+ */
+typedef struct __ELCD_HandleTypeDef
+{
+    LCDIF_Type *Instance;                                                      /*!< LCDIF_Instance  */
+    ELCD_Init_t Init;                                                          /*!< LCDIF Init parameters      */
+    ELCD_IT_Config_t IT_Config;                                                /*!< LCDIF interrupt configuration parameters      */
+    const uint8_t *pTxBuffPtr;                                                 /*!< Pointer to LCDIF Tx transfer Buffer */
+    uint16_t TxXferSize;                                                       /*!< LCDIF Tx Transfer size              */
+    __IO uint16_t TxXferCount;                                                 /*!< LCDIF Tx Transfer Counter           */
+    uint8_t *pRxBuffPtr;                                                       /*!< Pointer to LCDIF Rx transfer Buffer */
+    uint16_t RxXferSize;                                                       /*!< LCDIF Rx Transfer size              */
+    __IO uint16_t RxXferCount;                                                 /*!< LCDIF Rx Transfer Counter           */
+    void (*TxHalfCpltCallback)(struct __ELCD_HandleTypeDef *hELCD);            /*!< LCDIF Tx Half Complete Callback        */
+    void (*TxCpltCallback)(struct __ELCD_HandleTypeDef *hELCD);                /*!< LCDIF Tx Complete Callback             */
+    void (*RxHalfCpltCallback)(struct __ELCD_HandleTypeDef *hELCD);            /*!< LCDIF Rx Half Complete Callback        */
+    void (*RxCpltCallback)(struct __ELCD_HandleTypeDef *hELCD);                /*!< LCDIF Rx Complete Callback             */
+    void (*ErrorCallback)(struct __ELCD_HandleTypeDef *hELCD);                 /*!< LCDIF Error Callback                   */
+    void (*AbortCpltCallback)(struct __ELCD_HandleTypeDef *hELCD);             /*!< LCDIF Abort Complete Callback          */
+    void (*AbortTransmitCpltCallback)(struct __ELCD_HandleTypeDef *hELCD);     /*!< LCDIF Abort Transmit Complete Callback */
+    void (*AbortReceiveCpltCallback)(struct __ELCD_HandleTypeDef *hELCD);      /*!< LCDIF Abort Receive Complete Callback  */
+    void (*WakeupCallback)(struct __ELCD_HandleTypeDef *hELCD);                /*!< LCDIF Wakeup Callback                  */
+    void (*RxFifoFullCallback)(struct __ELCD_HandleTypeDef *hELCD);            /*!< LCDIF Rx Fifo Full Callback            */
+    void (*TxFifoEmptyCallback)(struct __ELCD_HandleTypeDef *hELCD);           /*!< LCDIF Tx Fifo Empty Callback           */
+    void (*RxEventCallback)(struct __ELCD_HandleTypeDef *hELCD, uint16_t Pos); /*!< LCDIF Reception Event Callback     */
+    void (*MspInitCallback)(struct __ELCD_HandleTypeDef *hELCD);               /*!< LCDIF Msp Init callback                */
+    void (*MspDeInitCallback)(struct __ELCD_HandleTypeDef *hELCD);             /*!< LCDIF Msp DeInit callback              */
+} ELCD_HandleTypeDef;
 
 
 
 
 
+void eLCDIF_Init(void);
 
-
-
-
-
-
+void BSP_eLCDIF_Init(ELCD_HandleTypeDef *ElcdHandle);
 
 
 
